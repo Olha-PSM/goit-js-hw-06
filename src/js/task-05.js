0d229f1b1f9f4f09dca42c118feda5085f7c3126
Olha-PSM <Olha84pismennaa@gmail.com>
2023-05-24 10:51:45 +0300

    update js

---
+++ src/js/task-05.js
@@ -0,0 +1,15 @@
+const textInput = document.querySelector('#name-input')
+const textOutput = document.querySelector('#name-output')
+
+console.log(textInput, textOutput)
+
+textInput.addEventListener("input", (event) => {
+    textOutput.textContent = event.currentTarget.value;
+    if (event.currentTarget.value === "") {
+        textOutput.textContent = "Anonymous"
+    }
+    
+});
+ 
+
+
