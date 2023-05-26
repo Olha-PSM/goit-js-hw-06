aff328dc8cb19195e3d1acb7455639f985560184
Olha-PSM <Olha84pismennaa@gmail.com>
2023-05-26 11:10:26 +0300

    delete

--- src/js/task-01.js
+++
@@ -1,9 +0,0 @@
-const categoriesEl = document.querySelectorAll('li.item');
-console.log(`Number of categories: ${categoriesEl.length}`);
-
-categoriesEl.forEach(categorie =>
-  console.log(
-    `Category: ${categorie.firstElementChild.textContent}
-    Elements: ${categorie.lastElementChild.children.length}`,
-  ),
-);
--- src/js/task-07.js
+++
@@ -1,7 +0,0 @@
-const inputEl = document.querySelector("#font-size-control");
-const textEl = document.querySelector("#text");
-console.log(inputEl, textEl)
-
-inputEl.addEventListener("input", (event) => {
-  textEl.style.fontSize = `${event.target.value}px`
-});
